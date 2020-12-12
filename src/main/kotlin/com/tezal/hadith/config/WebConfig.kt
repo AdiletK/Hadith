b26e7e7659d6ef4621f0d3c39eda5d86aa9a5292
Adik <adi.kadyrbekov.98@gmail.com>
2020-12-12 20:15:51 +0600

    fixed swagger config

--- src/main/kotlin/com/tezal/hadith/config/WebConfig.kt
+++ src/main/kotlin/com/tezal/hadith/config/WebConfig.kt
@@ -1,16 +1,15 @@
-package com.tezal.hadith.config;
+package com.tezal.hadith.config
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("http://localhost:8080")
-                .allowedHeaders("*").allowedOriginPatterns("*");
+                .allowedHeaders("*").allowedOriginPatterns("*")
     }
 }
