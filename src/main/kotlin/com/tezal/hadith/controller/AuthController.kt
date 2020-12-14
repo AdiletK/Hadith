6019e6e10d73d026d3fcf8980aa13464e8f0d944
Adik <adi.kadyrbekov.98@gmail.com>
2020-12-14 21:35:12 +0600

    change kt file

--- src/main/kotlin/com/tezal/hadith/controller/AuthController.kt
+++ src/main/kotlin/com/tezal/hadith/controller/AuthController.kt
@@ -1,28 +1,21 @@
-package com.tezal.hadith.controller;
+package com.tezal.hadith.controller
 
-import com.tezal.hadith.model.dto.UserDto;
-import com.tezal.hadith.service.AuthService;
-import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.*;
+import com.tezal.hadith.service.AuthService
+import io.swagger.annotations.ApiParam
+import com.tezal.hadith.model.dto.UserDto
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/auth")
-public class AuthController {
-    private final AuthService authService;
-
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
+class AuthController(private val authService: AuthService) {
     @PostMapping("/signin")
-    public String login(@ApiParam("Username") @RequestParam String username,
-                        @ApiParam("Password") @RequestParam String password) {
-        return authService.signin(username, password);
+    fun login(@ApiParam("Username") @RequestParam username: String?,
+              @ApiParam("Password") @RequestParam password: String?): String {
+        return authService.signin(username, password)
     }
+
     @PostMapping("/signup")
-    public String signUP(@RequestBody UserDto userDto) {
-        return authService.signUp(userDto);
+    fun signUP(@RequestBody userDto: UserDto?): String {
+        return authService.signUp(userDto)
     }
-
-
-
 }
